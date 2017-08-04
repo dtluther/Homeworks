91c93a0f83fcb4e099eb1545971d91c020f7525c
Dillon Luther <dillontluther@gmail.com>
2017-08-04 08:58:07 -0700

    Completed W1D5 Exercise 2: Queue class

--- W1D5.rb
+++ W1D5.rb
@@ -20,15 +20,51 @@ class Stack
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  stack = Stack.new
-  stack.add(3)
-  stack.add(2)
-  stack.add(7)
-  p stack.show
-  stack.remove
-  p stack.show
-  stack.add(1)
-  stack.add(5)
-  p stack.show
+# if __FILE__ == $PROGRAM_NAME
+#   stack = Stack.new
+#   stack.add(3)
+#   stack.add(2)
+#   stack.add(7)
+#   p stack.show
+#   stack.remove
+#   p stack.show
+#   stack.add(1)
+#   stack.add(5)
+#   p stack.show
+# end
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+    el
+  end
+
+  def dequeue
+    @queue.shift(el)
+  end
+
+  def show
+    @queue.dup
+  end
+
+end
+
+if $__FILE__ == $PROGRAM_NAME
+  queue = Queue.class
+  queue.enqueue(1)
+  queue.enqueue(2)
+  p queue.show
+  queue.dequeue
+  p queue.show
+  queue.enqueue(3)
+  queue.enqueue(4)
+  queue.enqueue(5)
+  p queue.show
+  queue.dequeue
+  p queue.show
 end
